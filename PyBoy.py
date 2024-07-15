fb8219090a5c61da6634cd3f423d0ee441583ec5
Jacob McGowan <73966737+TheDevAtlas@users.noreply.github.com>
2024-07-15 13:45:34 -0400

    New training stuff

--- PyBoy.py
+++ PyBoy.py
@@ -1,4 +1,7 @@
 # https://datacrystal.romhacking.net/wiki/The_Legend_of_Zelda:_Link%27s_Awakening_(Game_Boy)
+# https://datacrystal.romhacking.net/wiki/The_Legend_of_Zelda:_Link%27s_Awakening_(Game_Boy)/RAM_map
+
+
 
 import gymnasium as gym
 from gymnasium import spaces
@@ -38,7 +41,7 @@ class GenericPyBoyEnv(gym.Env):
             self.pyboy.button(actions[action])
 
         # Consider disabling renderer when not needed to improve speed:
-        # self.pyboy.tick(1, False)
+        #self.pyboy.tick(1, False)
         self.pyboy.tick(1)
 
         done = self.pyboy.game_wrapper.game_over
@@ -46,19 +49,60 @@ class GenericPyBoyEnv(gym.Env):
         self._calculate_fitness()
         reward=self._fitness-self._previous_fitness
 
-        observation=self.pyboy.game_area()
+        observation = self.pyboy.game_area()
         info = {}
         truncated = False
 
         return observation, reward, done, truncated, info
 
     def _calculate_fitness(self):
-        self._previous_fitness=self._fitness
+        self._previous_fitness = self._fitness
 
-        # NOTE: Only some game wrappers will provide a score
-        # If not, you'll have to investigate how to score the game yourself
-        # Inventory + New Screens #
-        self._fitness = 
+        inventoryValues = 0
+
+        # Inventory memory locations
+        inventory_start = 0xDB02
+        inventory_end = 0xDB0B
+        
+        # Loop through the inventory memory range and add the values to inventoryValues
+        for address in range(inventory_start, inventory_end + 1):
+            if self.pyboy.memory[address] > 0:
+                inventoryValues += 7
+
+        # World map status memory locations
+        visit_start = 0xD800
+        visit_end = 0xD8FF
+
+        for address in range(visit_start, visit_end + 1):
+            byte_value = self.pyboy.memory[address]
+            if byte_value & 0x80:  # visited
+                inventoryValues += 15
+            if byte_value & 0x20:  # owl talked
+                inventoryValues += 10
+            if byte_value & 0x10:  # changed from initial status
+                inventoryValues += 5
+
+        # Other significant memory locations and values
+        if self.pyboy.memory[0xDB0C] == 0x01:  # Flippers
+            inventoryValues += 20
+        if self.pyboy.memory[0xDB0D] == 0x01:  # Potion
+            inventoryValues += 20
+        if self.pyboy.memory[0xDB43] > 0:  # Power bracelet level
+            inventoryValues += 10 * self.pyboy.memory[0xDB43]
+        if self.pyboy.memory[0xDB44] > 0:  # Shield level
+            inventoryValues += 10 * self.pyboy.memory[0xDB44]
+        if self.pyboy.memory[0xDB49] > 0:  # Ocarina songs
+            inventoryValues += 5 * bin(self.pyboy.memory[0xDB49]).count('1')
+        if self.pyboy.memory[0xDB4D] > 0:  # Number of bombs
+            inventoryValues += 2 * self.pyboy.memory[0xDB4D]
+        if self.pyboy.memory[0xDB45] > 0:  # Number of arrows
+            inventoryValues += 2 * self.pyboy.memory[0xDB45]
+        if self.pyboy.memory[0xDB5D] > 0 or self.pyboy.memory[0xDB5E] > 0:  # Number of rupees
+            inventoryValues += self.pyboy.memory[0xDB5D] + self.pyboy.memory[0xDB5E]
+
+        # Update the fitness value
+        self._fitness += inventoryValues
+
 
     def reset(self, **kwargs):
         self.pyboy.game_wrapper.reset_game()
@@ -81,10 +125,10 @@ def run_bot(index):
     env = GenericPyBoyEnv(pyboy, debug=False)
     observation, info = env.reset()
     
-    for _ in range(100000):  # Run for a fixed number of steps or until done
+    for _ in range(60000):  # Run for a fixed number of steps or until done
         action = env.action_space.sample()  # Replace with your action selection logic
         observation, reward, done, truncated, info = env.step(action)
-        pyboy.tick()
+        #pyboy.tick()
         #if done:
             #break
         
@@ -93,7 +137,7 @@ def run_bot(index):
 
 if __name__ == "__main__":
     processes = []
-    for i in range(1):  # Number of bots you want to run
+    for i in range(6):  # Number of bots you want to run
         p = multiprocessing.Process(target=run_bot, args=(i,))
         processes.append(p)
         p.start()
