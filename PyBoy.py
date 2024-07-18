bd064de71d1cb9a33f0a34893acf6d4048a488ce
Jacob McGowan <73966737+TheDevAtlas@users.noreply.github.com>
2024-07-18 11:47:46 -0400

    update

--- PyBoy.py
+++ PyBoy.py
@@ -173,7 +173,7 @@ def run_bot(index):
 
 if __name__ == "__main__":
     processes = []
-    for i in range(6):  # Number of bots you want to run
+    for i in range(1):  # Number of bots you want to run
         p = multiprocessing.Process(target=run_bot, args=(i,))
         processes.append(p)
         p.start()
